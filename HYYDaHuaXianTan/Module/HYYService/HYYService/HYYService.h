824fb7f6298e602ede6707ddad7fddfa32bb577c
hu <>
2019-05-28 16:38:32 +0800

    添加自定义库

---
+++ HYYDaHuaXianTan/Module/HYYService/HYYService/HYYService.h
@@ -0,0 +1,13 @@
+//
+//  HYYService.h
+//  HYYService
+//
+//  Created by hu on 2019/5/28.
+//  Copyright © 2019 huyiyong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HYYService : NSObject
+
+@end
---
+++ HYYDaHuaXianTan/Module/HYYService/HYYService/HYYService.m
@@ -0,0 +1,13 @@
+//
+//  HYYService.m
+//  HYYService
+//
+//  Created by hu on 2019/5/28.
+//  Copyright © 2019 huyiyong. All rights reserved.
+//
+
+#import "HYYService.h"
+
+@implementation HYYService
+
+@end
